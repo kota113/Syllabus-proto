86c8fe46d8dfec9a6bb2e251529daba9819afddd
kota113 <kota113@keio.jp>
2024-03-13 17:13:10 +0900

    少しリファクタリング

--- assets/csvToJSON.py
+++ assets/csvToJSON.py
@@ -2,24 +2,28 @@ import csv
 import json
 import re
 
+
 def extract_day(value):
     day_match = re.search(r'([月火水木金土日])', value)
     return day_match.group() if day_match else None
 
+
 def extract_numeric(value):
     numeric_part = re.search(r'\d+', value)
     return int(numeric_part.group()) if numeric_part else None
+
 
 def convert_csv_to_json(csv_file_path, output_json_path):
     json_data = []
 
     with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
+        row: dict
         for row in reader:
-            #授業形態を今まで分割
+            # 授業形態を今まで分割
             styles = [{"style": style.strip()} for style in row['style'].split(',')]
 
-            #エラーハンドリング用
+            # エラーハンドリング用
             schedules = []
             days = row['day'].split(',')
             periods = row['period'].split(',')
@@ -29,7 +33,7 @@ def convert_csv_to_json(csv_file_path, output_json_path):
                 period_value = extract_numeric(day_period[1].strip()) if day_period[1] else None
                 schedules.append({"day": day_value, "period": period_value})
 
-            #単位数を数字のみ抽出
+            # 単位数を数字のみ抽出
             credit = extract_numeric(row['credit'])
             is_giga = str(row['is_giga']).strip().lower() == 'true'
 
@@ -68,5 +72,6 @@ def convert_csv_to_json(csv_file_path, output_json_path):
         json.dump(json_data, jsonfile, ensure_ascii=False, indent=2)
 
     print(f"Conversion completed. JSON data saved to {output_json_path}")
+
 
 convert_csv_to_json('/content/syllabus_data.csv', 'output.json')
