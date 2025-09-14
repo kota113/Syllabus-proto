8671c5e0793aaddda82475fb85fbd62b933172dd
kota-yata <kota@yatagai.com>
2025-09-14 18:16:09 +0900

    Remove leftovers from previous version

--- scripts/utils.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-# calculate year and semester
-def get_year(): return str(datetime.now().year)
-# 春休みの始まり(2月)から春学期の終わり(7月)まではspring、それ以外はfall
-def get_semester(): return "spring" if 2 <= datetime.now().month <= 7 else "fall"
